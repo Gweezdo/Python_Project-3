cfe8bcec6f3f32626b23a91a8560589ac6531886
Gweezdo <z.trivium@gmail.com>
2020-04-01 07:14:02 +1100

    Defined winOrLose function Fleshed out game logic Updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Python_Project-3
 Udemy Assignment - Python Tic Tac Toe game
+
+Milestone Project 1
+Congratulations on making it to your first milestone!
+You've already learned a ton and are ready to work on a real project.
+
+Your assignment: Create a Tic Tac Toe game. You are free to use any IDE you like.
+
+Here are the requirements:
+
+2 players should be able to play the game (both sitting at the same computer)
+The board should be printed out every time a player makes a move
+You should be able to accept input of the player position and then place a symbol on the board
+Feel free to use Google to help you figure anything out (but don't just Google "Tic Tac Toe in Python" otherwise you won't learn anything!) Keep in mind that this project can take anywhere between several hours to several days.
+
+There are parts of this that will be a struggle...and that is good! I have complete faith that if you have made it this far through the course you have all the tools and knowledge to tackle this project. Remember, it's totally open book, so take your time, do a little research, and remember:
+
+HAVE FUN!
--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,4 +1,5 @@
-XorO = {
+# Dictionary to record player inputs
+myDict = {
     '1':' ',
     '2':' ',
     '3':' ',
@@ -10,27 +11,112 @@ XorO = {
     '9':' '
 }
 
+# Prints gameboard to console
 def printBoard():
+    print('\n'*20)
     print("     |     |     ")
-    print("  " + XorO['7'] + "  " + "|" + "  " + XorO['8'] + "  " + "|" + "  " + XorO['9'] + "  ")
-    print("     |     |     ")
-    print('-----------------')
-    print("     |     |     ")
-    print("  " + XorO['4'] + "  " + "|" + "  " + XorO['5'] + "  " + "|" + "  " + XorO['6'] + "  ")
+    print("  " + myDict['7'] + "  " + "|" + "  " + myDict['8'] + "  " + "|" + "  " + myDict['9'] + "  ")
     print("     |     |     ")
     print('-----------------')
     print("     |     |     ")
-    print("  " + XorO['1'] + "  " + "|" + "  " + XorO['2'] + "  " + "|" + "  " + XorO['3'] + "  ")
+    print("  " + myDict['4'] + "  " + "|" + "  " + myDict['5'] + "  " + "|" + "  " + myDict['6'] + "  ")
     print("     |     |     ")
+    print('-----------------')
+    print("     |     |     ")
+    print("  " + myDict['1'] + "  " + "|" + "  " + myDict['2'] + "  " + "|" + "  " + myDict['3'] + "  ")
+    print("     |     |     ")
+    print('\n'*1)
+    
 
-printBoard()
+# printBoard()
 
+def winOrLose(myDict):
+    tRow = [myDict['7'], myDict['8'], myDict['9']]
+    mRow = [myDict['4'], myDict['5'], myDict['6']]
+    bRow = [myDict['1'], myDict['2'], myDict['3']]
+    lCol = [myDict['7'], myDict['4'], myDict['1']]
+    mCol = [myDict['8'], myDict['5'], myDict['2']]
+    rCol = [myDict['9'], myDict['6'], myDict['3']]
+    diagDown = [myDict['7'], myDict['5'], myDict['3']]
+    diagUp = [myDict['9'], myDict['5'], myDict['1']]
+    totalList = tRow + mRow + bRow
+
+    if tRow.count('X') == 3 or tRow.count('O') == 3:
+        print(f"Congratulations! '{tRow[0]}' is the winner!")
+        return True
+    elif mRow.count('X') == 3 or mRow.count('O') == 3:
+        print(f"Congratulations! '{mRow[0]}' is the winner!")
+    elif bRow.count('X') == 3 or bRow.count('O') == 3:
+        print(f"Congratulations! '{bRow[0]}' is the winner!")
+    elif lCol.count('X') == 3 or lCol.count('O') == 3:
+        print(f"Congratulations! '{lCol[0]}' is the winner!")
+    elif mCol.count('X') == 3 or mCol.count('O') == 3:
+        print(f"Congratulations! '{mCol[0]}' is the winner!")
+    elif rCol.count('X') == 3 or rCol.count('O') == 3:
+        print(f"Congratulations! '{rCol[0]}' is the winner!")
+    elif diagDown.count('X') == 3 or diagDown.count('O') == 3:
+        print(f"Congratulations! '{diagDown[0]}' is the winner!")
+    elif diagUp.count('X') == 3 or diagUp.count('O') == 3:
+        print(f"Congratulations! '{diagUp[0]}' is the winner!")
+    else:
+        count = 0
+        for item in totalList:
+            if item.isalpha():
+                count+=1
+                if count == 9:
+                    print("It's a Draw")
+
+
+
+
+
+
+# Game Start
 print("Welcome to Tic Tac Toe")
-
 player1 = input("Type 'X' or 'O' to choose you fighter: ")
+
+if player1.lower() == 'x':
+    player2 = 'O'
+else:
+    player2 = 'X'    
+
 print("Player 1 will go first")
 print("Ready Player 1? Enter 'Yes' or 'No': ")
 startGame = input()
 
+if startGame.lower() == 'yes':
+    printBoard()
+    while True:
+        print("Choose your next position: (1-9)")
+        p1 = input()
+        while int(p1) >= 10:
+            print("Please type in 'X'")
+            p1 = input()
+        
+        myDict[p1] = player1.upper()
+        printBoard()
+        winOrLose(myDict)
+        
+        if winOrLose == True:
+            break
 
-player2 = input()
+        print("Choose your next position: (1-9)")
+        p2 = input()
+        while int(p2) >= 10:
+            print("Please type in 'O'")
+            p2 = input()
+        
+        myDict[p2] = player2
+        printBoard()
+        winOrLose(myDict)
+        if winOrLose == True:
+            break
+elif startGame.lower() == 'no':
+    print("Goodbye")
+else:
+    print("Something")
+
+
+    
+
+    
